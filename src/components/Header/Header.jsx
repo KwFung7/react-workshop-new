9dec367d01a77298e7e5e6f23eb94db62a834ede
KwFxng <kwfxng@gmail.com>
2020-04-24 17:57:04 +0800

    update Header to detect scroll

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,31 @@ import { Button } from 'antd';
 import { LogoutOutlined } from '@ant-design/icons';
 
 class Header extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      whiteTheme: false
+    };
+  }
+
+  handleScroll = () => {
+    this.setState({
+      whiteTheme: window.pageYOffset > 0
+    });
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
   render() {
     return (
       <header className="workshop-header">
-        <div className="top-header white-theme">
+        <div className={`top-header ${this.state.whiteTheme ? 'white-theme' : ''}`}>
           <div className="header-wrapper">
             <div className="title">IES Training</div>
             <div className="header-menu">
