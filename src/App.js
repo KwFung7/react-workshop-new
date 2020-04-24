0d32d5cdc2ec40e71c768ce0fdb2f29a3399d859
KwFxng <kwfxng@gmail.com>
2020-04-24 21:45:09 +0800

    Routing control

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import EmployeePage from "./pages/EmployeePage/EmployeePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
-    <div>
-      {/*<EmployeePage />*/}
-      <LoginPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/employees"/>
+        </Route>
+        <Route path="/employees">
+          <EmployeePage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route render={() => <div>404 Not Found</div>} />
+      </Switch>
+    </Router>
   );
 }
 
